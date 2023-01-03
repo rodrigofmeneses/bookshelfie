72e491da0fce76d55b488405b26d7e8afc9c5aa2
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2023-01-03 14:35:16 -0300

    feat: :sparkles: Models to author and book resources

---
+++ src/models/book.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema(
+  {
+    id: { type: String },
+    title: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'authors',
+      required: true
+    },
+    cover_url: { type: String },
+    pageNumber: { type: Number }
+  },
+  {
+    versionKey: false
+  }
+)
+
+const books = mongoose.model('books', bookSchema)
+
+export default books;
